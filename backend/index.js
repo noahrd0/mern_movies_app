f4a2a9a9e99825a051098496d265bae0cebb8be8
noahrd0 <noahradal636@gmail.com>
2025-04-15 15:41:40 +0200

    login/logout/register back + front done

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from 'cors';
 import connectDB from './config/database.js';
 import userRoutes from './routes/UserRoutes.js';
 
@@ -7,6 +8,13 @@ dotenv.config();
 
 const app = express();
 const PORT = 5000;
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
